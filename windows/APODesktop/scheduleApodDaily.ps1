f88e90a03a4ff1391c77bb379fb8093433df6145
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2023-09-03 21:14:34 -0700

    feat(windows): Update wallpaper if on battery
    Summary:
    Hopefully this program doesn't have a large impact on battery

--- windows/APODesktop/scheduleApodDaily.ps1
+++ windows/APODesktop/scheduleApodDaily.ps1
@@ -1,9 +1,10 @@
 $apod =  Convert-Path .\APODesktop\bin\Debug\net7.0\APODesktop.exe
 $action = New-ScheduledTaskAction -Execute $apod
-$trigger = New-ScheduledTaskTrigger -Daily -At '12:45 PM'
+$trigger = New-ScheduledTaskTrigger -Daily -At '12:00 PM'
 $principal = New-ScheduledTaskPrincipal -UserId (whoami)
-$settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable
+$settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -StartWhenAvailable -AllowStartIfOnBatteries
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
 $name = 'APOD-Update Wallpaper daily'
 $description = 'from https://github.com/vegerot/APODesktop'
-Register-ScheduledTask 'SetApodWallpaper' -InputObject $task
+
+Register-ScheduledTask $name -InputObject $task
