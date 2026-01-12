2ff41c1201a62632dad386e2b5242476170577e9
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2026-01-12 14:24:47 -0800

    refactor(concurrentSequence): replace concurrent map implementation with task group
    Use TaskGroup for better performance and maintainability when processing concurrent operations

--- macOS/APODesktop/concurrentSequence.swift
+++ macOS/APODesktop/concurrentSequence.swift
@@ -9,20 +9,21 @@ extension Sequence {
   func concurrentCompactMap<T>(
     _ transform: @escaping (Element) async throws -> T?
   ) async rethrows -> [T] {
-    // concurrent
-    let tasks = self.map({ e in
-      Task {
-        try await transform(e)
+    try await withThrowingTaskGroup(of: (Int, T?).self) { group in
+      for (index, element) in self.enumerated() {
+        group.addTask {
+          (index, try await transform(element))
+        }
       }
-    })
 
-    // map
-    var results = [Result<T?, Error>]()
-    for task in tasks {
-      results.append(await task.result)
+      var indexedResults: [(Int, T?)] = []
+      for try await result in group {
+        indexedResults.append(result)
+      }
+
+      return indexedResults
+        .sorted(by: { lhs, rhs in lhs.0 < rhs.0 })
+        .compactMap { element in element.1 }
     }
-
-    //compact
-    return results.compactMap({ result in try? result.get() })
   }
 }
