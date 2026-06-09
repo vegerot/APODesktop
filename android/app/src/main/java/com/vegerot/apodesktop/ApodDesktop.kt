248e9557561fc6b1d5c8d9e5272fba92fb54e9f1
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2026-06-08 22:17:52 -0700

    refactor(android): remove unused import

--- android/app/src/main/java/com/vegerot/apodesktop/ApodDesktop.kt
+++ android/app/src/main/java/com/vegerot/apodesktop/ApodDesktop.kt
@@ -6,7 +6,6 @@ import android.app.PendingIntent
 import android.app.WallpaperManager
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.Dispatchers
